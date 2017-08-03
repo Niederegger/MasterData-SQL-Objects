9c9e01c79d625956af6557eeeeb5ad23d5a05081
Niederegger <thorsten.niederegger@vv.de>
2017-08-03 21:58:29 +0200

    Neues Feld MV_ID_NUM in vv_mastervalues

--- Tabelle vv_mastervalues.sql
+++ Tabelle vv_mastervalues.sql
@@ -2,16 +2,19 @@
 -- 27.04.2017 KB: MV_SOURCE_ID eingefügt und statt MV_SOURCE jetzt MV_DATA_ORIGIN. MV_FIELDNAME verlängert.
 -- 07.05.2017 KB: Added field MV_UPLOAD_ID to identify each upload-batch 
 -- 08.06.2017 KB; Added field MV_LAST_SEEN, removed MV_DATA_ORIGIN and MV_URLSOURCE (now in vv_uploads table)
+-- 03.08.2017 KB: MV_ID_NUM hinzugefügt und Index-struktur angepasst. Im Kommentar sind Hilfen zur Datenerhaltung
 
 Use MasterData
 go
 
 if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[vv_mastervalues]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-drop table [dbo].vv_mastervalues
+-- hier auskommentiert, ist sonst gefährlich
+-- drop table [dbo].vv_mastervalues
 GO
 
-CREATE TABLE dbo.vv_mastervalues
+CREATE TABLE dbo.vv_mastervalues 
 (
+  MV_ID_NUM       int identity constraint vv_mastervalues_index1 primary key,  -- identity fängt bei 1 an und erhöht um 1, primary key = unique und not null
   MV_SOURCE_ID    char(8),            -- z.B. DBAG für "Deutsche Böse AG", definiert u.a. welche Felder kommen. Könnte zukünftig auch "USER" für Benutzeränderungen nach WIKI-Methodik enthalten.
   MV_UPLOAD_ID    int,                -- a counter which increases by one for each upload-batch
   MV_ISIN         char(12) NOT NULL,  -- ISIN des Wertpapiers, zB DE0007100000
@@ -25,29 +28,32 @@ CREATE TABLE dbo.vv_mastervalues
 )
   
 
-CREATE  CLUSTERED  INDEX vv_mastervalues_index1 ON dbo.vv_mastervalues(mv_timestamp, MV_ISIN, MV_FIELDNAME) WITH  FILLFACTOR = 95 -- clustered nach Timestamp, d.h. wächst i.d.R. nur am Ende weiter
+CREATE  INDEX vv_mastervalues_index2 ON dbo.vv_mastervalues(mv_timestamp, MV_ISIN, MV_FIELDNAME) WITH  FILLFACTOR = 95 -- clustered nach Timestamp, d.h. wächst i.d.R. nur am Ende weiter
 GO
 
-CREATE  INDEX vv_mastervalues_index2 ON dbo.vv_mastervalues(MV_ISIN, MV_MIC, MV_FIELDNAME, MV_AS_OF_DATE) WITH  FILLFACTOR = 90 -- für Kurssuche
+CREATE  INDEX vv_mastervalues_index3 ON dbo.vv_mastervalues(MV_ISIN, MV_MIC, MV_FIELDNAME, MV_AS_OF_DATE) WITH  FILLFACTOR = 90 -- für Kurssuche
 GO
 
-CREATE  INDEX vv_mastervalues_index3 ON dbo.vv_mastervalues(MV_ISIN, MV_FIELDNAME) WITH  FILLFACTOR = 90  -- für Suche nach anderen Feldern
-GO
+--CREATE  INDEX vv_mastervalues_index3 ON dbo.vv_mastervalues(MV_ISIN, MV_FIELDNAME) WITH  FILLFACTOR = 90  -- für Suche nach anderen Feldern
+--GO
 
 -- GRANT SELECT, UPDATE ON [dbo].[fvs_analyse_ergebnis]  TO [Verwalten]
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
 /*   sp_spaceused vv_mastervalues
 
-select top 99 * from vv_mastervalues where MV_MIC is null
+select top 99 * from vv_mastervalues order by MV_ID_NUM desc
 
---- Daten erhalten, wenn Tabelle neu CREATED wird:
-select * into vv_mastervalues_bak20170608 from vv_mastervalues
+------- Daten erhalten, wenn Tabelle neu CREATED wird: ---------
+select * into vv_mastervalues_bak20170803 from vv_mastervalues
 
+-- und zurückholen:
 insert vv_mastervalues 
   select MV_SOURCE_ID, MV_UPLOAD_ID, MV_ISIN, MV_MIC,   MV_AS_OF_DATE ,  MV_FIELDNAME  ,  MV_TIMESTAMP  ,MV_LAST_SEEN,  MV_STRINGVALUE, MV_COMMENT  
-from vv_mastervalues_bak20170608 
+from vv_mastervalues_bak20170803 
+order by MV_TIMESTAMP
 
+----------------------------------------------------------------
 select count(*) from vv_mastervalues
 select count(*) from vv_mastervalues_bak20170608 
 select top 9 * from vv_mastervalues_bak20170608 
